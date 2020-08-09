c7b46521db47f1287a05c900d4f52f35c3f718d2
Brielle McCormick <briellemccormick@Brielles-MBP.fios-router.home>
2020-08-09 11:45:25 -0400

    adjusting env connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 // import { createConnection } from "mysql";
 var mysql = require("mysql");
-require("dotenv").config();
+var env = require("dotenv").config();
 var connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection(process.env.production);
+  connection = mysql.createConnection(process.env.DB_HOST);
+  console.log(password);
 }
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+});
+
 //       {
 //     host: "localhost",
 //     port: 8080,
@@ -15,8 +23,4 @@ if (process.env.JAWSDB_URL) {
 //     database: "burger_db",
 //   }
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-  }
-});
+// mysql://wrr6kg9pac934do8:sba86w4lhjhyz02g@rnr56s6e2uk326pj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/w7uw6d5rchjoensn
